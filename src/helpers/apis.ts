412f0ba17f76233292dd29483cf747047d01c2cd
agustin <agupalavecino97@gmail.com>
2023-02-12 20:39:03 -0300

    connect with api

--- src/helpers/apis.ts
+++ src/helpers/apis.ts
@@ -1,4 +1,5 @@
-const SERVER_URL = 'http://localhost:3789';
+// const SERVER_URL = 'http://localhost:3789';
+const SERVER_URL = 'https://cr23-api.onrender.com';
 
 export const APIs = {
     LOGIN: `${SERVER_URL}/signin`,
